3d732f4e60c609c7cb1a2d831c04479f63dc51f7
Valeriy0993 <137831411+Valeriy0993@users.noreply.github.com>
2023-11-14 23:15:58 +0200

    Home work js 7 (1)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+function renderGallery() {
+  gallery.innerHTML = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `
+    )
+    .join("");
+}
+
+renderGallery();
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("gallery__image")) {
+    const imageUrl = e.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+      <img src="${imageUrl}" width="800" height="600">
+    `);
+
+    instance.show();
+  }
+});
 
 console.log(galleryItems);
