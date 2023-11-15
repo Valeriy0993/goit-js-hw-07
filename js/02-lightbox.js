9f7860011cf0fbe71a96ed2e7895b7ec75a03a1f
Valeriy0993 <137831411+Valeriy0993@users.noreply.github.com>
2023-11-15 20:11:52 +0200

    Home work js 7 (2)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+function renderGallery() {
+  gallery.innerHTML = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `
+    )
+    .join("");
+}
+
+renderGallery();
+
+// Після відображення галереї ініціалізуємо SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
